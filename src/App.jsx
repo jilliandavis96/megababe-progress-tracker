34f32217328c2f00d1fdb2c9f959fba24b109799
jillian.davis96 <jilliandavis96@outlook.com>
2026-03-18 19:37:19 -0500

    add Header component with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ClassForm from "./components/ClassForm";
+import ClassList from "./components/ClassList";
+import About from "./components/About";
+import "./App.css";
+
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [classes, setClasses] = useState([]);
+
+  const addClass = (newClass) => {
+    setClasses([newClass, ...classes]);
+  };
+
+  const deleteClass = (id) => {
+    setClasses(classes.filter((c) => c.id !== id));
+  };
+
   return (
-    <div>
-      <h1>Hello, LaunchCode!</h1>
+    <div className="app">
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+
+      {currentPage === "home" ? (
+        <main>
+          <ClassForm addClass={addClass} />
+          <ClassList classes={classes} deleteClass={deleteClass} />
+        </main>
+      ) : (
+        <About />
+      )}
+
+      <Footer />
     </div>
   );
 }
